70e0c16051b2ba867c5e60c2117063e3dc0d02b7
Nate Beaty <nate@clixel.com>
2021-03-30 11:57:34 -0400

    lo siento, i change it back to root:root! local credentials battle

--- config/datastores.js
+++ config/datastores.js
@@ -49,7 +49,7 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     adapter: 'sails-mysql',
-    url: 'mysql://root:@localhost:3306/fb_project_hub_local',
+    url: 'mysql://root:root@localhost:3306/fb_project_hub',
 
   },
 
