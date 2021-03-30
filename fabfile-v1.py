94568fa34e4d18352fe7b51d6048dbd23a1db68e
Nate Beaty <nate@clixel.com>
2021-03-30 13:52:54 -0400

    v2 fabfile

---
+++ fabfile-v1.py
@@ -0,0 +1,26 @@
+from fabric.api import *
+
+env.hosts = ['firebelly.opalstacked.com']
+env.user = 'firebelly'
+env.shell = '/bin/bash -lic' # interactive shell to source .bashrc
+env.path = '~/Firebelly/fb-project-hub'
+env.remotepath = '/home/firebelly/apps/fb-project-hub/fb-project-hub'
+env.git_branch = 'deploy'
+env.warn_only = True
+
+def deploy():
+  update()
+  restart()
+
+def prepare():
+  local('npx sails run deploy')
+
+def update():
+  with cd(env.remotepath):
+    run('git fetch --all && git checkout --force origin/{0}'.format(env.git_branch))
+    run('npm install &>/dev/null')
+
+def restart():
+  with cd(env.remotepath):
+    run('../stop')
+    run('../start')
--- fabfile.py
+++ fabfile.py
@@ -1,26 +1,25 @@
-from fabric.api import *
+from fabric import task
+from invoke import run as local
 
-env.hosts = ['firebelly.opalstacked.com']
-env.user = 'firebelly'
-env.shell = '/bin/bash -lic' # interactive shell to source .bashrc
-env.path = '~/Firebelly/fb-project-hub'
-env.remotepath = '/home/firebelly/apps/fb-project-hub/fb-project-hub'
-env.git_branch = 'deploy'
-env.warn_only = True
+remote_path = "/home/firebelly/apps/fb-project-hub/fb-project-hub"
+remote_hosts = ["firebelly@firebelly.opalstacked.com"]
+git_branch = "deploy"
 
-def deploy():
-  update()
-  restart()
+# deploy
+@task(hosts=remote_hosts)
+def deploy(c):
+    update(c)
+    restart(c)
 
-def prepare():
-  local('npx sails run deploy')
+def update(c):
+    c.run("cd {} && git fetch --all && git checkout --force origin/{}".format(remote_path, git_branch))
+    c.run("cd {} && npm install &>/dev/null".format(remote_path))
 
-def update():
-  with cd(env.remotepath):
-    run('git fetch --all && git checkout --force origin/{0}'.format(env.git_branch))
-    run('npm install &>/dev/null')
+def restart(c):
+    c.run("cd {} && ../stop".format(remote_path))
+    c.run("cd {} && ../start".format(remote_path))
 
-def restart():
-  with cd(env.remotepath):
-    run('../stop')
-    run('../start')
+# local commands
+@task
+def prepare(c):
+    local("npx sails run deploy")
